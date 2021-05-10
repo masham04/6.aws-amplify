62cef1267df61e0affccdc6f006ea439385d2293
Muhammad Masham <66207787+masham04@users.noreply.github.com>
2021-05-10 15:07:45 +0500

    new

---
+++ gatsby-frontend/src/graphql/queries.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getTodos = /* GraphQL */ `
+  query GetTodos {
+    getTodos {
+      id
+      title
+      done
+    }
+  }
+`;
--- gatsby-frontend/src/pages/index.js
+++ gatsby-frontend/src/pages/index.js
@@ -1,5 +1,70 @@
-import React from "react"
+import React, { useState, useRef, useEffect } from "react"
+import { addTodo } from "../graphql/mutations"
+import { getTodos } from "../graphql/queries"
+import { API } from "aws-amplify"
+import shortid from "shortid"
+
 
 export default function Home() {
-  return <div>Hello Gatsbyjs</div>
+  const [loading, setLoading] = useState(true)
+  const [todoData, setTodoData] = useState(null)
+  const todoTitleRef = useRef("")
+
+  const addTodoMutation = async () => {
+    try {
+      const todo = {
+        id: shortid.generate(),
+        title: todoTitleRef.current.value,
+        done: false,
+      }
+      const data = await API.graphql({
+        query: addTodo,
+        variables: {
+          todo: todo,
+        },
+      })
+      todoTitleRef.current.value = ""
+      fetchTodos()
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const fetchTodos = async () => {
+    try {
+      const data = await API.graphql({
+        query: getTodos,
+      })
+      setTodoData(data)
+      setLoading(false)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading ...</h1>
+      ) : (
+        <div>
+          <label>
+            Todo:
+            <input ref={todoTitleRef} />
+          </label>
+          <button onClick={() => addTodoMutation()}>Create Todo</button>
+          {todoData.data &&
+            todoData.data.getTodos.map((item, ind) => (
+              <div style={{ marginLeft: "1rem", marginTop: "2rem" }} key={ind}>
+                {item.title}
+              </div>
+            ))}
+        </div>
+      )}
+    </div>
+  )
 }
